cfad7f5e0dd85850c9480bf80e91835f8bdf4581
SubhanRzayev <subhanrzayev97@gmail.com>
2022-08-03 03:15:56 +0400

    JavaTravelBook

---
+++ app/src/main/java/com/subhanrzayev/javamaps/roomdb/PlaceDao.java
@@ -0,0 +1,27 @@
+package com.subhanrzayev.javamaps.roomdb;
+
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.subhanrzayev.javamaps.model.Place;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Flowable;
+
+@Dao
+public interface PlaceDao {
+    @Query("SELECT * FROM Place")
+    Flowable<List<Place>> getAll();
+
+    @Insert
+    public Completable insert(Place place);
+
+    @Delete
+    public Completable delete(Place place);
+
+}
